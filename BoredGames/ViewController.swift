4c4fd69fc017287c3520ce5bbd8c747dc96442cc
Isaiah Smith <jordankid93@gmail.com>
2016-01-29 16:38:15 -0500

    Add Initial Code

---
+++ BoredGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoredGames
+//
+//  Created by Isaiah Smith on 1/29/16.
+//  Copyright © 2016 Isaiah Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
